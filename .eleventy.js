29f47903378b67aadf0d6770775facdce7760b57
Mac Martine <mac.martine@gmail.com>
2022-12-30 14:14:04 +0100

    Styles

--- .eleventy.js
+++ .eleventy.js
@@ -4,10 +4,15 @@ const eleventyNavigationPlugin = require("@11ty/eleventy-navigation");
 
 module.exports = function(eleventyConfig) {
 
+
   eleventyConfig.addPlugin(eleventyNavigationPlugin);
   eleventyConfig.addPassthroughCopy('src/assets', 'assets')
   eleventyConfig.addPassthroughCopy('src/media')
 	eleventyConfig.addPassthroughCopy("src/admin/config.yml");
+
+	eleventyConfig.addPassthroughCopy({
+		'./node_modules/alpinejs/dist/cdn.js': 'assets/scripts/alpine.js',
+	})
 
   const {
     DateTime
